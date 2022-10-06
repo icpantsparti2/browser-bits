4fe98fd3317b2a97f717ec2545721f12cb3aff73
icpantsparti2 <101484718+icpantsparti2@users.noreply.github.com>
2022-10-06 02:50:50 +0000

    v2022.10.05

--- firefox-style/chrome_debugger_profile/user.js
+++ firefox-style/chrome_debugger_profile/user.js
@@ -1,10 +1,20 @@
 /*******************************************************************************
-** [FIREFOX-PROFILE-FOLDER]/chrome_debugger_profile/user.js
-********************************************************************************
-** File/Update  : https://raw.githubusercontent.com/icpantsparti/browser-bits/main/firefox-style/chrome_debugger_profile/user.js
-** Version      : 2021.09.02
-** License (MIT): https://raw.githubusercontent.com/icpantsparti/browser-bits/main/LICENSE
-** Project      : https://github.com/icpantsparti/browser-bits
-** Disclaimer   : Use with care at your own risk, and verify any results
-*******************************************************************************/
+/* [FIREFOX-PROFILE-FOLDER]/chrome_debugger_profile/user.js
+/*------------------------------------------------------------------------------
+/* File/Update  : https://raw.githubusercontent.com/icpantsparti2/browser-bits/main/firefox-style/chrome_debugger_profile/user.js
+/* Version      : 2022.10.05
+/* License (MIT): https://raw.githubusercontent.com/icpantsparti2/browser-bits/main/LICENSE
+/* Project      : https://github.com/icpantsparti2/browser-bits
+/* Disclaimer   : Use with care at your own risk
+/******************************************************************************/
+//user_pref("devtools.editor.autoclosebrackets", false);
+//user_pref("devtools.inspector.showUserAgentStyles", true);
+//user_pref("devtools.selfxss.count", 5);
+//user_pref("devtools.theme", "dark");
+//user_pref("devtools.webconsole.groupWarningMessages", false);
+//user_pref("devtools.webconsole.input.autocomplete", false);
+//user_pref("devtools.webconsole.input.eagerEvaluation", false);
+//user_pref("devtools.webconsole.input.editor", true);
+//user_pref("devtools.webconsole.input.editorOnboarding", false);
+//user_pref("devtools.webconsole.input.editorWidth", 200);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
