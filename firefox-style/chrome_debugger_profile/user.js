ae0542bf4a0fc70985a2df1fbfbec5883b91a6e8
icpantsparti2 <101484718+icpantsparti2@users.noreply.github.com>
2023-10-24 04:19:00 +0000

    v2023.10.23
    + added: "devtools.styleeditor.showAtRulesSidebar"
    * removed some "//" prefixes

--- firefox-style/chrome_debugger_profile/user.js
+++ firefox-style/chrome_debugger_profile/user.js
@@ -2,19 +2,20 @@
 /* [FIREFOX-PROFILE-FOLDER]/chrome_debugger_profile/user.js
 /*------------------------------------------------------------------------------
 /* File/Update  : https://raw.githubusercontent.com/icpantsparti2/browser-bits/main/firefox-style/chrome_debugger_profile/user.js
-/* Version      : 2022.10.05
+/* Version      : 2023.10.23
 /* License (MIT): https://raw.githubusercontent.com/icpantsparti2/browser-bits/main/LICENSE
 /* Project      : https://github.com/icpantsparti2/browser-bits
 /* Disclaimer   : Use with care at your own risk
 /******************************************************************************/
 //user_pref("devtools.editor.autoclosebrackets", false);
 //user_pref("devtools.inspector.showUserAgentStyles", true);
-//user_pref("devtools.selfxss.count", 5);
+user_pref("devtools.selfxss.count", 5);
+user_pref("devtools.styleeditor.showAtRulesSidebar", false);
 //user_pref("devtools.theme", "dark");
 //user_pref("devtools.webconsole.groupWarningMessages", false);
 //user_pref("devtools.webconsole.input.autocomplete", false);
 //user_pref("devtools.webconsole.input.eagerEvaluation", false);
 //user_pref("devtools.webconsole.input.editor", true);
-//user_pref("devtools.webconsole.input.editorOnboarding", false);
+user_pref("devtools.webconsole.input.editorOnboarding", false);
 //user_pref("devtools.webconsole.input.editorWidth", 200);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
