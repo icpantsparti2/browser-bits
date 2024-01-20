ba17f413eb080b69fcb63e06e891d1e3933fe263
icpantsparti2 <101484718+icpantsparti2@users.noreply.github.com>
2024-01-20 15:11:34 +0000

    v2024.01.20.alpha.1

--- userscript/all_-_copy_link_on_click.user.js
+++ userscript/all_-_copy_link_on_click.user.js
@@ -1,35 +1,43 @@
 // ==UserScript==
-// @name          Link To Clipboard
-// @description   Prevent default opening of links. Double-click or middle-click links to copy them.
-// @version       2023.02.14
-// @match         https://www.messenger.com/*
-// @grant         GM_setClipboard
+// @name         all - Copy Link On Click
+// @namespace    Violentmonkey Scripts
+// @match        http*://*/*
+// @version      2024.01.20.alpha.1
+// @description  click links to copy them
+// @license      MIT
+// @grant        GM_setClipboard
 // ==/UserScript==
 
-/***************************************
-  2022.03.17 https://www.reddit.com/r/userscripts/comments/tf75y6/comment/i0z28um/
-  2023.02.14 https://www.reddit.com/r/FirefoxAddons/comments/111h3g4/comment/j8iutou/
-***************************************/
+(function() {
 
-document.addEventListener('click', event => {
-	const closestAElement = event.target.closest('a');
-	if (closestAElement && closestAElement.href) {
-    event.preventDefault();
-	}
-});
- 
-document.addEventListener('dblclick', event => {
-	const closestAElement = event.target.closest('a');
-	if (closestAElement && closestAElement.href) {
-		event.preventDefault();
-    GM_setClipboard(closestAElement.href);
-	}
-});
- 
-document.addEventListener('auxclick', event => {
-	const closestAElement = event.target.closest('a');
-	if (closestAElement && closestAElement.href) {
-		event.preventDefault();
-    GM_setClipboard(closestAElement.href);
-	}
-});
+  /***************************************
+    File/Update  : https://github.com/icpantsparti2/browser-bits/blob/main/userscript/all_-_copy_link_on_click.user.js
+    License (MIT): https://raw.githubusercontent.com/icpantsparti2/browser-bits/main/LICENSE
+  ***************************************
+    TODO: this is alpha/experimental and likely to keep changing
+  ***************************************
+    thanks for the snippet/idea from:
+    2022.03.17 https://www.reddit.com/r/userscripts/comments/tf75y6/comment/i0z28um/
+    2023.02.14 https://www.reddit.com/r/FirefoxAddons/comments/111h3g4/comment/j8iutou/
+    2024.01.17 https://www.reddit.com/r/firefox/comments/198kv0o/
+    document.addEventListener('auxclick', event => {
+      const closestAElement = event.target.closest('a');
+      if (closestAElement && closestAElement.href) {
+        event.preventDefault();
+        GM_setClipboard(closestAElement.href);
+      }
+    });
+  ***************************************/
+
+  'use strict';
+
+  /* copy link on middle click */
+  document.addEventListener('auxclick', event => {
+    const closestAElement = event.target.closest('a');
+    if (closestAElement && closestAElement.href) {
+      event.preventDefault();
+      GM_setClipboard(closestAElement.href);
+    }
+  });
+
+})();
