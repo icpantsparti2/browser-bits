cbe5413a2272ce45bd95a0d5d000929ac07a7f7a
icpantsparti2 <101484718+icpantsparti2@users.noreply.github.com>
2024-01-20 14:10:41 +0000

    Update all_-_copy_link_on_click.user.js
    v2023.02.14 from https://www.reddit.com/r/FirefoxAddons/comments/111h3g4/comment/j8iutou/

--- userscript/all_-_copy_link_on_click.user.js
+++ userscript/all_-_copy_link_on_click.user.js
@@ -1,26 +1,35 @@
 // ==UserScript==
-// @name Double-click copy link
-// @description Double-click copy links them.
-// @namespace ale
-// @version 2022.03.17
-// @include *
-// @grant GM_setClipboard
+// @name          Link To Clipboard
+// @description   Prevent default opening of links. Double-click or middle-click links to copy them.
+// @version       2023.02.14
+// @match         https://www.messenger.com/*
+// @grant         GM_setClipboard
 // ==/UserScript==
 
 /***************************************
   2022.03.17 https://www.reddit.com/r/userscripts/comments/tf75y6/comment/i0z28um/
+  2023.02.14 https://www.reddit.com/r/FirefoxAddons/comments/111h3g4/comment/j8iutou/
 ***************************************/
 
 document.addEventListener('click', event => {
 	const closestAElement = event.target.closest('a');
 	if (closestAElement && closestAElement.href) {
-		event.preventDefault();
+    event.preventDefault();
 	}
 });
-
+ 
 document.addEventListener('dblclick', event => {
 	const closestAElement = event.target.closest('a');
 	if (closestAElement && closestAElement.href) {
-		GM_setClipboard(closestAElement.href);
+		event.preventDefault();
+    GM_setClipboard(closestAElement.href);
+	}
+});
+ 
+document.addEventListener('auxclick', event => {
+	const closestAElement = event.target.closest('a');
+	if (closestAElement && closestAElement.href) {
+		event.preventDefault();
+    GM_setClipboard(closestAElement.href);
 	}
 });
